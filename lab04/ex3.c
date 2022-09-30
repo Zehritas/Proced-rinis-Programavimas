99b2cfde2b0233fc8d4ae2f8fa48f50455a9693e
Marijonas <marijoniukas@gmail.com>
2022-09-30 16:40:01 +0300

    added ex3 from lab04

--- lab04/ex3.c
+++ lab04/ex3.c
@@ -3,14 +3,13 @@
 int main()
 {
   //variables
-  int a, b, c, fc, check;
+  int a, b, c, fc;
   //input
   printf("This program takes three not negative whole numbers\na, b, c, it then outputs c-th member of the fibonacci sequence.\n");
   printf("Input 3 whole numbers : a, b, c.\n");
-  //foolproof
-  //(a >= 0 || b >= 0 || c >= 0)
 
-  while (scanf("%d %d %d", &a, &b, &c) != 3 || (getchar() != '\n'))
+  //foolproof
+  while (scanf("%d %d %d", &a, &b, &c) != 3 || (a < 0 || b < 0 || c < 0))
   {
     printf("Please input whole numbers!\n");
     while(getchar() != '\n')
@@ -19,14 +18,14 @@ int main()
     }
   }
 
-  for (int i = 2; i < c; ++i)
+  //algorithm
+  for (int i = 2; i <= c; ++i)
   {
+    fc = a + b;
     a = b;
     b = fc;
-    fc = a + b;
   }
-  printf("%d", fc);
-
-
+  //output
+  printf("%dth number of this sequence is: %d", c, fc);
 
 }
